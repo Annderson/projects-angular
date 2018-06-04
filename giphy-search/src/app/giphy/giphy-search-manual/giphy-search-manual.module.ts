6fce2edf4128f9b87e0d477f6f257de2a232a93b
Anderson Lima <anderson.rl.tr@gmail.com>
2018-06-04 14:28:55 -0300

    create components

--- giphy-search/src/app/giphy/giphy-search-manual/giphy-search-manual.module.ts
+++ giphy-search/src/app/giphy/giphy-search-manual/giphy-search-manual.module.ts
@@ -4,11 +4,13 @@ import { GiphySearchManualComponent } from './giphy-search-manual.component';
 import { GiphySearchService } from '../giphy-search.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { GiphyListModule } from '../giphy-list/giphy-list.module';
 
 @NgModule({
     imports: [
       CommonModule,
       FormsModule,
+      GiphyListModule
     ],
     exports: [GiphySearchManualComponent],
     declarations: [GiphySearchManualComponent],
